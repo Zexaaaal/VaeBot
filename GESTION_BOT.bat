38200743d25215def725c0f1294898773916f7d8
Zexal <therealzexal@gmail.com>
2026-03-24 00:18:56 +0100

    yeah????????

--- GESTION_BOT.bat
+++ GESTION_BOT.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 color 0b
-title GESTIONNAIRE BOT QI (Quasar)
+title CENTRE DE CONTROLE BOT QI (vps1euro)
 
 set VPS_HOST=VPS-925156.ssh.vps1euro.fr
 set VPS_PORT=9739
@@ -11,69 +11,81 @@ set REMOTE_DIR=/root/discord_bot
 :menu
 cls
 echo ========================================================
-echo             CENTRE DE CONTROLE BOT QI (vps1euro)
+echo             CENTRE DE CONTROLE BOT QI (Quasar)
 echo ========================================================
 echo.
-echo  MOT DE PASSE : %VPS_PWD% (clic-droit pour coller)
-echo  IPV6 REELLE : 2001:0861:4a41:6910:b6ad:4ea7:6f58:8e90
+echo  MOT DE PASSE : %VPS_PWD% 
+echo  STATUS RESEAU: IPv6 ONLY (Restriction VPS 1€)
 echo.
 echo  [1] OUVRIR LA CONSOLE (SSH)
-echo  [2] ENVOYER LES FICHIERS ET REDEMARRER (PUSH + PM2)
-echo  [3] VOIR LES LOGS DU BOT (Dernieres erreurs)
-echo  [4] DEPANNAGE VOIX (Activer le pont NAT64 si besoin)
-echo  [5] QUITTER
+echo  [2] ENVOYER FICHIERS + MAJ + DEMARRER (Full Push)
+echo  [3] VOIR LES LOGS DU BOT (Dernières erreurs)
+echo  [4] JUSTE RELANCER LE BOT (PM2 Restart)
+echo  [5] ARRÊTER LE BOT (PM2 Stop)
+echo  [6] ENVOYER LE FICHIER .ENV (Secrets)
+echo  [7] ACTIVER LE PONT IPv6 (Dépannage Voix)
+echo  [8] QUITTER
 echo.
 echo ========================================================
-set /p choice=Choisis une option [1-5]: 
+set /p choice=Choisis une option [1-8]: 
 
 if "%choice%"=="1" goto ssh
-if "%choice%"=="2" goto push
+if "%choice%"=="2" goto fullpush
 if "%choice%"=="3" goto logs
-if "%choice%"=="4" goto nat64
-if "%choice%"=="5" exit
+if "%choice%"=="4" goto restart
+if "%choice%"=="5" goto stop
+if "%choice%"=="6" goto pushenv
+if "%choice%"=="7" goto nat64
+if "%choice%"=="8" exit
 goto menu
 
 :ssh
 cls
-echo ========================================================
-echo             OUVERTURE DE LA CONSOLE...
-echo ========================================================
 ssh root@%VPS_HOST% -p %VPS_PORT%
 pause
 goto menu
 
-:push
+:fullpush
 cls
-echo ========================================================
-echo             ENVOI DU CODE EN COURS... (SCP)
-echo ========================================================
 echo Creation du dossier si besoin...
 ssh root@%VPS_HOST% -p %VPS_PORT% "mkdir -p %REMOTE_DIR%"
-echo Envoi des fichiers locaux... (Veuillez coller le mdp si demande)
+echo Envoi des fichiers locaux...
 scp -P %VPS_PORT% -r ./* root@%VPS_HOST%:%REMOTE_DIR%/
-echo Redemarrage du bot... (npm install + pm2 restart)
+echo Installation des modules et démarrage...
 ssh root@%VPS_HOST% -p %VPS_PORT% "cd %REMOTE_DIR% && npm install && pm2 restart all || pm2 start src/index.js --name \"qi-bot\""
 echo.
-echo Termine !
+echo Termine ! Verifie les logs (Option 3).
 pause
 goto menu
 
 :logs
 cls
-echo ========================================================
-echo             LOGS DU BOT (Affichage en direct)
-echo ========================================================
 ssh root@%VPS_HOST% -p %VPS_PORT% "pm2 logs qi-bot --lines 50"
+pause
+goto menu
+
+:restart
+cls
+ssh root@%VPS_HOST% -p %VPS_PORT% "pm2 restart all || pm2 start %REMOTE_DIR%/src/index.js --name \"qi-bot\""
+pause
+goto menu
+
+:stop
+cls
+ssh root@%VPS_HOST% -p %VPS_PORT% "pm2 stop all"
+pause
+goto menu
+
+:pushenv
+cls
+scp -P %VPS_PORT% .env root@%VPS_HOST%:%REMOTE_DIR%/.env
+echo Done.
 pause
 goto menu
 
 :nat64
 cls
-echo ========================================================
-echo             ACTIVATION DU PONT NAT64 (IPV6)
-echo ========================================================
-ssh root@%VPS_HOST% -p %VPS_PORT% "echo -e \"nameserver 2a01:4f8:c2c:123b::1\nnameserver 2a00:1098:2c::1\" | sudo tee /etc/resolv.conf && ping -c 3 8.8.8.8"
-echo.
-echo Si vous avez vu des '64 bytes from 8.8.8.8', c'est que c'est gagne !
+echo Activation du pont DNS64 de Google...
+ssh root@%VPS_HOST% -p %VPS_PORT% "echo -e \"nameserver 2001:4860:4860::6464\nnameserver 2001:4860:4860::64\" | sudo tee /etc/resolv.conf && ping -c 3 ipv4.google.com"
 pause
 goto menu
