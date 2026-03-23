2e2ad8c931e399768c64c527e3f580099215a797
Zexal <therealzexal@gmail.com>
2026-03-23 17:41:22 +0100

    fix ipv6 udp loop

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first'); // FIX DISCORD VOICE UDP BUG ON IPV6 VPS
 const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
